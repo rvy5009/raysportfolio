1a9cde60ce42782a3cbc38d91b5e3401d8c9f8fb
ray <rvy5009@gmail.com>
2020-03-03 13:54:48 -0500

    Changed pdf download

--- src/components/about.js
+++ src/components/about.js
@@ -1,42 +1,45 @@
-import React, { Component } from "react"
-import Pdf from "../info/resume.pdf"
-import self from '../info/self.jpg'
+import React, { Component } from "react";
+import Pdf from "../info/resume.pdf";
+import self from "../info/self.jpg";
 class About extends Component {
   constructor(props) {
-    super(props)
-    this.state = {
-    }
+    super(props);
+    this.state = {};
   }
 
   render() {
     return (
       <div>
-        <h1 className="about">
-          About
-        </h1>
+        <h1 className="about">About</h1>
         <div className="myAbout">
           <div className="selfPicWrapper">
             <img src={self} alt="picturesd" className="selfPic" />
-          </div>  
+          </div>
           <div className="aboutWords">
-          I am a solutions-oriented and innovative Software Engineer who holds seven years’ experience leading the operations for a small business in the transportation industry. As both a Web Developer and Automotive Technician, I have an innate ability to self-learn complicated concepts and translate this to intuitive solutions for the teams I manage and the clients I support. I seek the opportunity to work in a collaborative, client-facing team that places consumer experience at the center of what they do.
-          </div>  
-          <a href={Pdf} download="Raymond Yen Resume">Resume</a>
+            I am a solutions-oriented and innovative Software Engineer who holds
+            seven years’ experience leading the operations for a small business
+            in the transportation industry. As both a Web Developer and
+            Automotive Technician, I have an innate ability to self-learn
+            complicated concepts and translate this to intuitive solutions for
+            the teams I manage and the clients I support. I seek the opportunity
+            to work in a collaborative, client-facing team that places consumer
+            experience at the center of what they do.
+          </div>
+          <a href="https://drive.google.com/u/0/uc?id=1_B_wca9VVRYsu38EZlskuzzjAUwbKwOI&export=download" target="_blank" download="Raymond Yen Resume">
+            Resume
+          </a>
           <div className="myContact">
-          <h2>
-            <a href="mailto:rvy5009@gmail.com" target=" ">
-              rvy5009@gmail.com
-            </a>
-          </h2>
-          <h3>
-            Cell Phone: 646-678-1190
-          </h3>
+            <h2>
+              <a href="mailto:rvy5009@gmail.com" target=" ">
+                rvy5009@gmail.com
+              </a>
+            </h2>
+            <h3>Cell Phone: 646-678-1190</h3>
+          </div>
         </div>
-        </div>
-
-      </div>  
-    )
+      </div>
+    );
   }
 }
 
-export default About
+export default About;
